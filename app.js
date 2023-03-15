e30a47ce70f34342026bd99266aadd3dd8cebf08
mattzager <mzager@redhat.com>
2023-03-15 16:49:09 -0500

    Update app.js
    removing read file and replacing with process.env

--- app.js
+++ app.js
@@ -34,19 +34,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-let configMap;
-let message;
+const greeting = process.env.GREETING;
 
 app.use('/api/greeting', (request, response) => {
   const name = (request.query && request.query.name) ? request.query.name : 'World';
 
-  if (!message) {
+  if (!greeting) {
     response.status(500);
     return response.send({ content: 'no config map' });
   }
 
   logger.debug('Replying to request, parameter={}', name);
-  return response.send({ content: message.replace(/%s/g, name) });
+  return response.send({ content: greeting.replace(/%s/g, name) });
 });
 
 // Add basic health check endpoints
@@ -57,42 +56,5 @@ app.use('/ready', (request, response) => {
 app.use('/live', (request, response) => {
   return response.sendStatus(200);
 });
-
-// Periodic check for config map update
-// If new configMap is found, then set new log level
-setInterval(() => {
-  retrieveConfigfMap().then(config => {
-    if (!config) {
-      message = null;
-      return;
-    }
-
-    if (JSON.stringify(config) !== JSON.stringify(configMap)) {
-      configMap = config;
-      message = config.message;
-
-      // Set New log level
-      if (logger.level !== config.level.toLowerCase()) {
-        logger.info('New configuration retrieved: {}', config.message);
-        logger.info('New log level: {}', config.level.toLowerCase());
-        logger.level = config.level.toLowerCase();
-      }
-    }
-  }).catch(error => {
-    logger.error('Error getting config', error);
-  });
-}, 2000);
-
-// Get ConfigMap Stuff
-const jsyaml = require('js-yaml');
-
-// Find the Config Map
-function retrieveConfigfMap () {
-  return readFile(process.env.NODE_CONFIGMAP_PATH, { encoding: 'utf8' }).then(configMap => {
-    // Parse the configMap, which is yaml
-    const configMapParsed = jsyaml.load(configMap);
-    return configMapParsed;
-  }).catch(error => logger.error(error));
-}
 
 module.exports = app;
